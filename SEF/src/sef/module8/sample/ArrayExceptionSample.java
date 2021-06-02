12e91e4740bb098f2acaf610689ac29cb7192fd2
TimG666 <timur.geldiev@gmail.com>
2021-06-02 17:34:22 +0300

    Timur_Geldiev

--- SEF/src/sef/module8/sample/ArrayExceptionSample.java
+++ SEF/src/sef/module8/sample/ArrayExceptionSample.java
@@ -2,21 +2,36 @@ package sef.module8.sample;
 // Needs to be completed
 public class ArrayExceptionSample {
 
-	public static void main(String arg[]){
-		
+	public static void main(String arg[]) {
+
 		//The valid indices for this array are from 0 to 4
 		int scores[] = new int[5];
-		
+		scores[0] = 0;
+		scores[1] = 1;
+		scores[2] = 2;
+		scores[3] = 3;
+
 		//But the the loop below will run from 0 to 5.
 		//Last index is 4 so accessing scores[5] will cause an exception
 		//1 - Create a for loop that runs from 0-5. Print elements of this array inside the loop.
-		for (int i=0; i <5; i++) {
-			System.out.println(scores[i]);
+		try {
+			for (int i = 0; i < 5; i++) {
+				System.out.println(scores[i]/i);
+				System.out.println(100 / 0);
+			}
 		}
-		
+		catch(ArrayIndexOutOfBoundsException ArrEx){
+				System.out.println("You r in catch block due arrayIndexOutOfBoundsException");
+			}
+		catch(ArithmeticException ArithEx){
+				System.out.println("You r in catch due to ArithmeticException");
+			}
+		}
+	}
+
 			
 		// The above loop is a logical error in the program which results in
 		// ArrayIndexOutOfBoundsException. This is an example of Unchecked Exception.
 		
-	}
-}
+
+
